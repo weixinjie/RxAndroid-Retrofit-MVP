8e006de91a1c5fb7d932930a7798fac94f0427f4
weixinjie <weixinjie123123@gmail.com>
2016-09-12 14:39:13 +0800

    add "flatMap、take、filter" operation to ElementaryPresenter

--- app/src/main/java/com/will/custom_rxandroid/presenter/elementary/ElementaryPresenter.java
+++ app/src/main/java/com/will/custom_rxandroid/presenter/elementary/ElementaryPresenter.java
@@ -1,14 +1,19 @@
 package com.will.custom_rxandroid.presenter.elementary;
 
 
+import android.util.Log;
+
 import com.will.custom_rxandroid.http.BaseApi;
 import com.will.custom_rxandroid.pojo.elementary.ZhuangBiImage;
 import com.will.custom_rxandroid.presenter.base.BasePresenter;
 
 import java.util.List;
 
+import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -24,9 +29,31 @@ public class ElementaryPresenter extends BasePresenter<ElementaryView> {
     public void search(String des, final boolean is_refresh, int current_page) {
         mvpView.show_loading();
 //        mvpView.show_toast("loading page---" + current_page); //only for debug
-        subscription = BaseApi.getZhuangBiApi().search(des)
+        subscription = BaseApi.getZhuangBiApi().search("装逼")
+//                .flatMap(new Func1<List<ZhuangBiImage>, Observable<ZhuangBiImage>>() { //这里的转化是像for循环一样一个一个的发送数据
+//                    @Override
+//                    public Observable<ZhuangBiImage> call(List<ZhuangBiImage> zhuangBiImages) {
+//                        return Observable.from(zhuangBiImages);
+//                    }
+//                })
+//                .take(3) //只取前三个(需要与from进行配合使用)
+//                .filter(new Func1<ZhuangBiImage, Boolean>() { //对得到的数据进行过滤,该数据符合发送的标准则返回true,如果该数据不符合返回的标准则返回false
+//                    @Override
+//                    public Boolean call(ZhuangBiImage zhuangBiImage) {
+//                        if (zhuangBiImage.getImage_url().contains("http://zhuangbi.idagou.com/i/2016-01-27-bd6f1e1e31fea64f7f2f33ba31b0b1a2.gif"))
+//                            return true;
+//                        else
+//                            return false;
+//                    }
+//                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Action1<ZhuangBiImage>() {
+//                    @Override
+//                    public void call(ZhuangBiImage zhuangBiImage) {
+//                        Log.e("-----url", zhuangBiImage.getImage_url());
+//                    }
+//                });
                 .subscribe(new Observer<List<ZhuangBiImage>>() {
                     @Override
                     public void onCompleted() {
