f310a6ad223617d4c95cc89b170a1a9a7836ffbd
weixinjie <weixinjie123123@gmail.com>
2016-09-19 14:54:30 +0800

    add ClickView

---
+++ app/src/main/java/com/will/custom_rxandroid/ui/custom_view/ClickView.java
@@ -0,0 +1,103 @@
+package com.will.custom_rxandroid.ui.custom_view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * Created by will on 16/9/19.
+ */
+
+public class ClickView extends View implements View.OnClickListener {
+    private Paint paint;
+    private Rect mBounds;
+
+    private int text_size = 30;
+    private int count = 0;
+
+    public ClickView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOnClickListener(this);
+        paint = new Paint();
+        mBounds = new Rect();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setColor(Color.WHITE);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+        paint.setColor(Color.RED);
+        paint.setTextSize(text_size);
+        String current_count = String.valueOf("当前数量" + count);
+        canvas.drawText(current_count, getWidth() / 2 - measureText()[0] / 2, getHeight() / 2 + measureText()[1] / 2, paint);
+    }
+
+    @Override
+    public void onClick(View v) {
+        count++;
+        invalidate();
+    }
+
+
+    /**
+     * 注意系统帮我们测量的高度和宽度都是MATCH_PARNET，当我们设置明确的宽度和高度时，
+     * 系统帮我们测量的结果就是我们设置的结果，
+     * 当我们设置为FILL_PARENT,或者MATCH_PARENT系统帮我们测量的结果就是MATCH_PARENT的长度So当我们设置wrap_content的时候
+     * View的宽跟高都需要我们自己设置
+     * <p>
+     * 重写之前先了解MeasureSpec的specMode,一共三种类型:
+     * **EXACTLY：一般是设置了明确的值或者是MATCH_PARENT
+     * **AT_MOST：表示子布局限制在一个最大值内，一般为WARP_CONTENT
+     * **UNSPECIFIED：表示子布局想要多大就多大，很少使用
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width_model = MeasureSpec.getMode(widthMeasureSpec);
+        int width_size = MeasureSpec.getSize(widthMeasureSpec);
+        int height_model = MeasureSpec.getMode(heightMeasureSpec);
+        int height_size = MeasureSpec.getSize(heightMeasureSpec);
+
+        int view_width;
+        int view_height;
+
+        if (width_model == MeasureSpec.EXACTLY) { //如果模式为精准的
+            view_width = getPaddingLeft() + getPaddingRight() + width_size;
+        } else {
+            view_width = (int) (measureText()[0] + getPaddingLeft() + getPaddingRight());
+        }
+
+        if (height_model == MeasureSpec.EXACTLY) {
+            view_height = getPaddingTop() + getPaddingBottom() + height_size;
+        } else {
+            view_height = (int) measureText()[1] + getPaddingBottom() + getPaddingTop();
+        }
+        setMeasuredDimension(view_width, view_height);
+    }
+
+    /**
+     * 测量文字的宽度跟高度
+     *
+     * @return
+     */
+    private float[] measureText() {
+        paint.setTextSize(text_size);
+        String text = String.valueOf("当前数量" + count);
+        paint.getTextBounds(text, 0, text.length(), mBounds);
+        return new float[]{mBounds.width(), mBounds.height()};
+    }
+
+    public void clearText() {
+        count = 0;
+        invalidate();
+    }
+
+}
--- app/src/main/java/com/will/custom_rxandroid/ui/custom_view/CustomViewActivity.java
+++ app/src/main/java/com/will/custom_rxandroid/ui/custom_view/CustomViewActivity.java
@@ -2,13 +2,36 @@ package com.will.custom_rxandroid.ui.custom_view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.will.custom_rxandroid.R;
 
+import butterknife.BindInt;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class CustomViewActivity extends AppCompatActivity {
+
+    @BindView(R.id.click_view)
+    ClickView clickView;
+    @BindView(R.id.bt_clear)
+    Button bt_clear;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.bt_clear})
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_clear:
+                clickView.clearText();
+                break;
+        }
     }
 }
