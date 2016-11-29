8bba6fc68fdc7464b3dba76d3d53d90d0b4aa6c4
weixinjie <weixinjie123123@gmail.com>
2016-11-29 16:10:19 +0800

    update WebActivity.java

--- app/src/main/java/com/will/custom_rxandroid/ui/web/WebActivity.java
+++ app/src/main/java/com/will/custom_rxandroid/ui/web/WebActivity.java
@@ -2,24 +2,64 @@ package com.will.custom_rxandroid.ui.web;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.will.custom_rxandroid.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class WebActivity extends AppCompatActivity {
 
-    @BindView(R.id.wb_detail)
-    WebView wb_detail;
+    @BindView(R.id.lv_content)
+    ListView lv_content;
+
+    List<String> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
-        ButterKnife.bind(this);
+        data = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            data.add("content" + i);
+        }
+    }
 
-        wb_detail.loadUrl("file:///android_asset/demo.html");
+    class MAdapter extends BaseAdapter {
+        List data;
+
+        public MAdapter(List<String> data) {
+            this.data = data;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            return null;
+        }
     }
 }
